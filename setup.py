8be59ad2c5384f4f239dddc4d2f84fb09b3c08d8
jamespltan <32437481+jamespltan@users.noreply.github.com>
2020-04-19 23:45:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='sppa',
-    version='1.0.7',
+    version='1.0.8',
     packages=['sppa'],
     install_requires=['numpy', 'pulp'],
     url='https://github.com/jamespltan/sppa',
-	download_url='https://github.com/jamespltan/sppa/archive/v1.0.7.tar.gz',
+	download_url='https://github.com/jamespltan/sppa/archive/v1.0.8.tar.gz',
     license='MIT',
     author='James Tan',
     author_email='jamestan@ntu.edu.sg',
